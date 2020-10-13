fb970b434c1a6e1ea38984d9e3e4728efd09e14d
Michelle Yu <Michelle.P.Yu.19@dartmouth.edu>
2020-10-12 21:25:13 -0400

    began homepage style, add background, update stateless components

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,48 +1,72 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { WelcomeText } from './Text';
+import Faq from 'react-faq-component';
+import { FaqContent, WelcomeText } from './Text';
+import './HomepageStyles.scss';
 
-class Homepage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const styles = {
+  bgColor: 'clear',
+  titleTextColor: 'black',
+  rowTitleColor: 'black',
+  // rowContentColor: 'grey',
+  arrowColor: 'white',
+};
 
-    };
-  }
-
-  render() {
-    const renderIntro = () => {
-      return (
-        <div>
-          <h2>What is for future reference?</h2>
-          <WelcomeText />
-          <div />
-        </div>
-      );
-    };
-    const renderReadyToTry = () => {
-      return (
-        <div>
-          <h2>Ready to Try?</h2>
-          <div>
-            <nav>
-              <ul style={{ listStyleType: 'none' }}>
-                <li><NavLink to="/self" exact>Send a Message to My Future Self</NavLink></li>
-                <li><NavLink to="/lovedOne">Send a Message to a Loved One</NavLink></li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      );
-    };
+const Homepage = () => {
+  const renderWelcome = () => {
     return (
-      <div>
-        <h1>For Future Reference</h1>
-        {renderIntro()}
-        {renderReadyToTry()}
+      <div id="welcomeTagline">
+        <div>Reflect, Remember, </div>
+        <div>and Repeat</div>
       </div>
     );
-  }
-}
+  };
+  const renderInfo = () => {
+    return (
+      <WelcomeText />
+    );
+  };
+
+  const renderReadyToStart = () => {
+    return (
+      <div>
+        <h2>Ready to Try?</h2>
+        <div>
+          <nav>
+            <ul style={{ listStyleType: 'none' }}>
+              <li><NavLink to="/self" exact>Send a Message to My Future Self</NavLink></li>
+              <li><NavLink to="/lovedOne">Send a Message to a Loved One</NavLink></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  };
+  const renderBlog = () => {
+    return (
+      <div>
+        What are others saying?
+        <p>Check out our blog to read what other people are reflecting on during this time.</p>
+        <p>Read More</p>
+      </div>
+    );
+  };
+  const renderFaq = () => {
+    return (
+      <div>
+        <Faq data={FaqContent} styles={styles} />
+      </div>
+    );
+  };
+  return (
+    <div id="homepage">
+      <div id="welcomeContainer">{renderWelcome()}</div>
+      <div className="curved" id="infoContainer">{renderInfo()}</div>
+      <div id="startContainer">{renderReadyToStart()}</div>
+      <div id="blogContainer">{renderBlog()}</div>
+      <div id="faqContainer">{renderFaq()}</div>
+    </div>
+  );
+};
 
 export default Homepage;
--- src/components/Text.js
+++ src/components/Text.js
@@ -6,8 +6,7 @@ export const WelcomeText = () => {
   return (
     <div>
       <div>
-        <p>in the pace of this world, we sometimes struggle to keep up especially in times (Covid, economy, BLM) understanding this is our natural instinct to keep up w the pace,
-        we wanted to provide a tool to be able to capture a certain moment(takeaways, influences, thoughts )
+        <p> A tool to capture the present, remember the past, and refresh your outlook on the future.
         </p>
         <p>But how? In order to prevent us from hopping on the next quick thing,
         for future reference not only captures but is also a reminder of the current circumstances in order to strengthen us and one another.
@@ -16,6 +15,36 @@ export const WelcomeText = () => {
       </div>
     </div>
   );
+};
+
+// Homepage faq
+export const FaqContent = {
+  title: 'FAQ',
+  rows: [
+    {
+      title: 'How do I know my info is safe?',
+      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat, 
+              ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus. 
+              In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae. 
+              Fusce sed commodo purus, at tempus turpis.`,
+    },
+    {
+      title: 'Where will I recieve my email?',
+      content:
+        'Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.',
+    },
+    {
+      title: 'Curabitur laoreet, mauris vel blandit fringilla',
+      content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem. 
+            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam. 
+            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat. 
+            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+    },
+    {
+      title: 'What is the package version',
+      content: 'v1.0.0',
+    },
+  ],
 };
 
 export const otherText = () => {
